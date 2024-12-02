1b491f55035a22fef3e3841c086f7ab77bae04a1
Ethan Frick <novafocks@protonmail.com>
2024-12-02 16:30:28 -0700

    Closer to working!

--- airfoil/coefficients.m
+++ airfoil/coefficients.m
@@ -1,6 +1,5 @@
 % This is to rewrite the code because the other ones didn't work!
 
-
 %% Clear Workspace
 
 clear; clc; close all;
@@ -20,108 +19,70 @@ raw.naca = readmatrix("ClarkY14_NACA_TR628.xlsx");
 
 %% Step 1 - Getting Constants
 
-%%% COLLECTED %%%
-
 % Pressure matrix
-data.c.p_M = raw.collected(:, 15:end);
+const.c.p_M = raw.collected(:, 15:end);
 
 % Atmospheric pressure
-data.c.pressure_atm = raw.collected(:, 2);
+const.c.pressure_atm = raw.collected(:, 2);
 
 % Density
-data.c.rho = raw.collected(:, 3);
+const.c.rho = raw.collected(:, 3);
 
 % Airspeed
-data.c.airspeed = raw.collected(:, 4);
+const.c.airspeed = raw.collected(:, 4);
 
 % Dynamic pressure
-data.c.q_inf = raw.collected(:, 5);
+const.c.q_inf = raw.collected(:, 5);
 
 % Angles of attack
-data.c.aoa = raw.collected(:, 8);
+const.c.aoa = raw.collected(:, 8);
 
 % UNIQUE angles of attack
-data.c.unique_aoa = unique(data.c.aoa);
+const.c.unique_aoa = unique(const.c.aoa);
 
 % Number of ports
-data.c.num_ports = 16;
+const.c.num_ports = 16;
 
 %%% PORTS %%%
 
 % NUMBER %
-data.p.num          = raw.ports(:, 1);
-data.p.num_ports    = data.p.num(~isnan(data.p.num));
+const.p.num          = raw.ports(:, 1);
+const.p.num_ports    = const.p.num(~isnan(const.p.num));
 
 % X-POSITION %
-data.p.x            = raw.ports(:, 2);
-data.p.x_ports      = data.p.x(~isnan(data.p.num));
+const.p.x            = raw.ports(:, 2);
+const.p.x_ports      = const.p.x(~isnan(const.p.num));
 
 % Y-POSITION %
-data.p.y            = raw.ports(:, 3);
-data.p.y_ports      = data.p.y(~isnan(data.p.num));
+const.p.y            = raw.ports(:, 3);
+const.p.y_ports      = const.p.y(~isnan(const.p.num));
 
-% Modified for plotting with coef at AoA = 9
-data.p.y_mod = data.p.y;
-data.p.y_mod(end + 1) = 0;
+% Chord length definition
+const.c.cord_length = max(const.p.y);
 
 % Z-POSITION
-data.p.z            = raw.ports(:, 4);
-data.p.z_ports      = data.p.z(~isnan(data.p.num));
+const.p.z            = raw.ports(:, 4);
+const.p.z_ports      = const.p.z(~isnan(const.p.num));
 
 %%% NACA %%%
 
 % Angle of attack
-data.n.aoa = raw.naca(:, 1);
+const.n.aoa = raw.naca(:, 1);
 
 % Coefficient of lift
-data.n.coefL = raw.naca(:, 2);
+const.n.coefL = raw.naca(:, 2);
 
 % Coefficient of drag
-data.n.coefD = raw.naca(:, 3);
+const.n.coefD = raw.naca(:, 3);
 
-%% Step 2 - Coefficients of Pressure
-
-% Making new matrix of correct size
-[misc.p_M_rows, misc.p_M_cols] = size(data.c.p_M);
-coef.pressure_full = zeros([misc.p_M_rows misc.p_M_cols]);
-
-% Calculating each value of coefficients of pressure
-for i = 1 : misc.p_M_cols
-
-    % Pressure matrix divided by dynamic pressure
-    coef.pressure_full(:, i) = data.c.p_M(:, i) ./ ...
-                                (data.c.q_inf);
-
-end
-
-%% Step 3 - Average the Pressure
-
-% Create matrix of coefficients of pressure
-coef.pressure = zeros([length(data.c.unique_aoa) data.c.num_ports]);
-
-% Calculate coefficients of pressure
-for i = 1:length(data.c.unique_aoa)
-
-    % Get the indices where the angle of attack matches the current angle
-    idx = (data.c.aoa == data.c.unique_aoa(i));
-    
-    % Average the pressure data for those indices
-    coef.pressure(i, :) = mean(coef.pressure_full(idx, :), 1);
-
-end
-
-%% Step 4 - Plot Ports for AoA = 9
+%% Step 2 - Plot Ports for AoA = 9
 
 % Collect pressures for AoA = 9
-coef.pressure_aoa9 = coef.pressure(data.c.unique_aoa == 9, :);
-
-% Redefining some values for sake of graph
-coef.pressure_aoa9(end + 1) = 0;
-coef.pressure_aoa9(end + 1) = coef.pressure_aoa9(1);
+coef.pressure_aoa9 = c_P_calc(9, const);
 
 % Plot over y
 figure();
-plot(data.p.y_mod, coef.pressure_aoa9);
+plot(const.p.y, coef.pressure_aoa9);
 
 % Plot metadata
 xlabel('Normalized Chord Position');
@@ -131,44 +92,102 @@ title('Average C_P Distribution for AoA of 9 degrees');
 % Flip plot
 set(gca, 'YDir', 'reverse');
 
-%% Step 5 - Calculating C_L and C_D
+%% Step 3 - Calculating C_L and C_D
 
-% Chord length definition
-data.c.cord_length = max(data.p.y);
+for i = 1 : length(const.c.unique_aoa)
+    % Defining current angle of attack and pressures
+    misc.curr_aoa       = const.c.unique_aoa(i);
+    misc.curr_pressure  = c_P_calc(misc.curr_aoa, const);
 
-% Averaging coefficients of pressure
-for i = 1 : data.c.num_ports
-    coef.pressure_avg(i) = mean(coef.pressure(:, i));
+    % Calculating coefficient of normal and axial force
+    coef.normal(i) = - (1 / const.c.cord_length) * trapz(const.p.y, misc.curr_pressure);
+    coef.axial(i) = (1 / const.c.cord_length) * trapz(const.p.z, misc.curr_pressure);
+
+    % Calculating coefficients of lift and drag
+    coef.lift(i) = (coef.normal(i) * cosd(misc.curr_aoa)) ...
+                    - (coef.axial(i) * sind(misc.curr_aoa));
+    coef.drag(i) = (coef.normal(i) * sind(misc.curr_aoa)) ...
+                    + (coef.axial(i) * cosd(misc.curr_aoa));
 end
 
-% Calculating coefficient of normal and axial force
-coef.normal = - (1 / data.c.cord_length) * trapz(data.p.y_ports, coef.pressure_avg);
-coef.axial = (1 / data.c.cord_length) * trapz(data.p.z_ports, coef.pressure_avg);
-
-% Calculating coefficients of lift and drag
-for i = 1 : length(data.c.unique_aoa)
-    misc.curr_aoa = data.c.unique_aoa(i);
-
-    coef.lift(i) = (coef.normal * cosd(misc.curr_aoa)) ...
-                    - (coef.axial * sind(misc.curr_aoa));
-    coef.drag(i) = (coef.normal * sind(misc.curr_aoa)) ...
-                    + (coef.axial * cosd(misc.curr_aoa));
-end
-
-%% Step 6 - Plotting C_L and C_D
+%% Step 4 - Plotting C_L and C_D
 
 figure();
-plot(data.c.unique_aoa, coef.lift);
+plot(const.c.unique_aoa, coef.lift);
 hold on;
-plot(data.n.aoa, data.n.coefL);
+plot(const.n.aoa, const.n.coefL);
 hold off;
 
 legend("Calculated", "NACA");
 
 figure();
-plot(data.c.unique_aoa, coef.drag);
+plot(const.c.unique_aoa, coef.drag);
 hold on;
-plot(data.n.aoa, data.n.coefD);
+plot(const.n.aoa, const.n.coefD);
 hold off;
 
 legend("Calculated", "NACA");
+
+%% Appendix - C_P Function
+
+function c_P = c_P_calc(aoa, const)
+
+    % Making new matrix of correct size
+    [misc.p_M_rows, misc.p_M_cols] = size(const.c.p_M);
+    coef.pressure_full = zeros([misc.p_M_rows misc.p_M_cols]);
+    
+    % Calculating each value of coefficients of pressure
+    for i = 1 : misc.p_M_cols
+    
+        % Pressure matrix divided by dynamic pressure
+        coef.pressure_full(:, i) = const.c.p_M(:, i) ./ ...
+                                    (const.c.q_inf);
+    
+    end
+
+    % Create matrix of coefficients of pressure
+    coef.pressure = zeros([length(const.c.unique_aoa) const.c.num_ports]);
+    
+    % Calculate coefficients of pressure
+    for i = 1 : length(const.c.unique_aoa)
+    
+        % Get the indices where the angle of attack matches the current angle
+        idx = (const.c.aoa == const.c.unique_aoa(i));
+        
+        % Average the pressure data for those indices
+        coef.pressure(i, :) = mean(coef.pressure_full(idx, :), 1);
+    
+    end
+
+    % Take coefficients of pressure for ports with specific AoA
+    coef.pressure_aoa = coef.pressure(const.c.unique_aoa == aoa, :);
+
+    
+    % Linear fit of pressure from ports 8 to 9 for estimation
+    linfit.port8to9_slope       = (coef.pressure_aoa(9) - coef.pressure_aoa(8)) / ...
+                                    (const.p.y_ports(9) - const.p.y_ports(8));
+    linfit.port8to9_intercept   = coef.pressure_aoa(8) - (linfit.port8to9_slope * const.p.y_ports(8));
+
+    % Linear fit of pressure from ports 10 to 11 for estimation
+    linfit.port10to11_slope     = (coef.pressure_aoa(11) - coef.pressure_aoa(10)) / ...
+                                    (const.p.y_ports(11) - const.p.y_ports(10));
+    linfit.port10to11_intercept = coef.pressure_aoa(10) - (linfit.port10to11_slope * const.p.y_ports(10));
+
+    
+    linfit.port8to9_estimation      = linfit.port8to9_intercept + ...
+                                    (linfit.port8to9_slope * const.c.cord_length);
+
+    linfit.port10to11_estimation    = linfit.port10to11_intercept + ...
+                                    (linfit.port10to11_slope * const.c.cord_length);
+
+    % Calculating pressure at trailing edge using estimations
+    const.c.p_TE                    = mean([linfit.port8to9_estimation, ...
+                                            linfit.port10to11_estimation]);
+
+    coef.pressure_aoa_wTE           = [coef.pressure_aoa(1:9) ...
+                                        const.c.p_TE ...
+                                        coef.pressure_aoa(10:end)];
+
+
+    c_P = coef.pressure_aoa_wTE;
+end
